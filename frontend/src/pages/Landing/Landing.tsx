bf3bef587d8f96196bbd909eef0a335c1f0bec44
Nicolas Pearson <nic.s.pearson@gmail.com>
2018-12-17 16:35:35 +0200

    Updated Landing Layout

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -53,49 +53,6 @@ class Landing extends React.Component<LandingProps, State> {
 							<AntDesign />
 						</section>
 					</section>
-					<section className="Why__Layout">
-						<section className="Text__Wrapper">
-							<h1>Why React</h1>
-							<p>
-								<span>
-									React makes it painless to create interactive UIs. Design simple views for each
-									state in your application, and React will efficiently update and render just the
-									right components when your data changes.
-								</span>
-							</p>
-							<section className="Text__Wrapper__Buttons">
-								<Button className="Button" type="ghost">
-									Learn More
-								</Button>
-							</section>
-						</section>
-						<section className="Image__Wrapper">
-							<ReactOfficial />
-						</section>
-					</section>
-					<section className="Why__Layout">
-						<section className="Image__Wrapper HiddenAtMd">
-							<AntDesign />
-						</section>
-						<section className="Text__Wrapper Text__Wrapper__Alt">
-							<h1>Why Ant Design</h1>
-							<p>
-								<span>
-									A design system with values of Nature and Determinacy for better user experience
-									of enterprise applications. Provides a React UI library that contains a set of
-									high quality components and demos for building rich, interactive user interfaces.
-								</span>
-							</p>
-							<section className="Text__Wrapper__Buttons">
-								<Button className="Button" type="ghost">
-									Learn More
-								</Button>
-							</section>
-						</section>
-						<section className="Image__Wrapper ShowFlexAtMd" style={{ display: 'none' }}>
-							<AntDesign />
-						</section>
-					</section>
 				</section>
 			</Page>
 		);
